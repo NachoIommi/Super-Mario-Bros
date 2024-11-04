9bf629b966d7ac9bbb7d18c82e14874a84d8d0ee
Santiago Vilas <santiagovilasrn@gmail.com>
2024-11-04 00:10:51 -0300

    fondos de cada nivel y pantalla ranking ajustada

--- MarioBros/src/GUI/PantallaRanking.java
+++ MarioBros/src/GUI/PantallaRanking.java
@@ -42,7 +42,7 @@ public class PantallaRanking extends JPanel{
     }
     
     public void agregarImagen() {
-		String ruta = "imagenesOriginales/img/ranking.jpeg";
+		String ruta = "imagenesOriginales/img/RANKING.jpg";
 		imagenInicio = new JLabel();
 		imagenInicio.setSize(ConstantesVistas.PANEL_ANCHO, ConstantesVistas.PANEL_ALTO);
 		ImageIcon icono = new ImageIcon(ruta);
